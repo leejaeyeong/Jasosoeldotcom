915edda8faa331c8066d3bccecd923251f8b5742
이재영 <dldustn14@gmail.com>
2020-09-11 16:27:35 +0900

    Add custom modelform code

--- main/forms.py
+++ main/forms.py
@@ -2,7 +2,20 @@ from django import forms
 from .models import Jasoseol
 
 class JssForm(forms.ModelForm):
-    # 어떤 모델과 대응되는 html form 
+
     class Meta:
         model = Jasoseol
         fields = ('title', 'content',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].label = "제목"
+        self.fields['content'].label = "자기소개서 내용"
+        self.fields['title'].widget.attrs.update({
+            'class': 'jss_title',
+            'placeholder': '제목',
+        })
+        self.fields['content'].widget.attrs.update({
+            'class': 'jss_content_form',
+        })
+        
